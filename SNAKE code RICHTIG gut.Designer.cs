7114f25a004dba4684bc93f662a66c720b1b7cf7
Damjan Krstic <D.Krstic.inf24@stud.bbbaden.ch>
2025-05-02 11:22:07 +0200

    Add files via upload

---
+++ SNAKE code RICHTIG gut.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace SNAKE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.START = new System.Windows.Forms.Button();
+            this.SNAP = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txtScore = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.gametimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // START
+            // 
+            this.START.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.START.Location = new System.Drawing.Point(589, 36);
+            this.START.Name = "START";
+            this.START.Size = new System.Drawing.Size(114, 56);
+            this.START.TabIndex = 0;
+            this.START.Text = "START";
+            this.START.UseVisualStyleBackColor = false;
+            this.START.Click += new System.EventHandler(this.StartGame);
+            // 
+            // SNAP
+            // 
+            this.SNAP.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.SNAP.Location = new System.Drawing.Point(589, 98);
+            this.SNAP.Name = "SNAP";
+            this.SNAP.Size = new System.Drawing.Size(114, 56);
+            this.SNAP.TabIndex = 1;
+            this.SNAP.Text = "SNAP";
+            this.SNAP.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.LightSlateGray;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(580, 680);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.UpdatePictureBoxGraphics);
+            // 
+            // txtScore
+            // 
+            this.txtScore.AutoSize = true;
+            this.txtScore.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.txtScore.Location = new System.Drawing.Point(602, 180);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(68, 20);
+            this.txtScore.TabIndex = 3;
+            this.txtScore.Text = "Score: 0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label2.Location = new System.Drawing.Point(602, 217);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "High Score";
+            // 
+            // gametimer
+            // 
+            this.gametimer.Tick += new System.EventHandler(this.GameTimerEvent);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(742, 708);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtScore);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.SNAP);
+            this.Controls.Add(this.START);
+            this.Name = "Form1";
+            this.Text = "SNAKE GAME";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyIsDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button START;
+        private System.Windows.Forms.Button SNAP;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label txtScore;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer gametimer;
+    }
+}
+
